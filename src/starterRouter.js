d0beab17cf29a9fb2eb0612c2366de6fee25c2c1
Ricardo <ricardschmidt@gmail.com>
2022-03-31 11:26:21 -0300

    first commit

--- src/starterRouter.js
+++ src/starterRouter.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Header from "./layout/starter/StarterHeader";
 import Footer from "./layout/starter/StarterFooter";
-import Starter from "./views/Starter.vue";
+import Starter from "./views/Index.vue";
 
 Vue.use(Router);
 
